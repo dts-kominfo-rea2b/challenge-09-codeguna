c99921bdd034808df0f8c61f0d8207795936dc0d
codeguna <pratamagunadhi@gmail.com>
2022-06-21 14:26:08 +0700

    finalized

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import './App.css';
+import Contact from './components/Contact';
+import Header from './components/Header';
 
 // Uncomment untuk memuat daftar kontak
-// import contacts from './data/contacts.json';
+import contacts from './data/contacts.json';
 
 const App = () => {
   // Masukkan Header dan Contact ke dalam div App
   return (
     <div className="App">
+      <Header></Header>
+      <br/>
+      <div id='contact-list'>
+        {contacts.map(contact => <Contact key={contact.email} data={contact} />)}
+      </div>
       
     </div>
   );
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,10 +1,21 @@
 // Terima props lalu tampilkan dalam Contact component
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
+import './Contact.css'
 
-const Contact = () => {
+const Contact = (props) => {
     return (
-        <></>
+        <div className='contact-box'>
+            <div className='contact-list'>
+            <img src={props.data.photo} className='contact-avatar' alt={props.data.name}></img>
+                <div className='contact-info'><h1 className='contact-name'>{props.data.name}</h1>
+                <p></p><a href='tel:{props.data.phone}' className='contact-phone'>{props.data.phone}</a>
+                <br/>
+                <a href='mailto:{props.data.email}' className='contact-email'>{props.data.email}</a>
+                </div>
+                
+            </div>
+        </div>
     )
 }
 
